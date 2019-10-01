5ea4d12705dd6039ba0cf03840dc7840ddb74e95
martinavit <martina.vit@cern.ch>
2019-10-01 11:49:30 +0200

    Update extractPuWeights.cc

--- src/extractPuWeights.cc
+++ src/extractPuWeights.cc
@@ -76,7 +76,7 @@ void extractPuWeights(const Sample& sample){
     std::shared_ptr<TH1D> puWeights[14][3];
    
     //categorization by year, run era and ucertainty
-    //const std::vector< std::string > eras2016 = {"2016Inclusive", "2016B", "2016C", "2016D", "2016E", "2016F", "2016G", "2016H"};
+    const std::vector< std::string > eras2016 = {"2016Inclusive", "2016B", "2016C", "2016D", "2016E", "2016F", "2016G", "2016H"};
     const std::vector< std::string > eras2017  = {"2017Inclusive", "2017B", "2017C", "2017D", "2017E", "2017F"};
     std::vector< std::string > allEras = eras2017;
     //allEras.insert(allEras.begin(), eras2017.begin(), eras2017.end() );
