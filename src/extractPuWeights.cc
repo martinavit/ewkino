40f3c2e3adb889df2db1a16cd19df3e1df6abed5
martinavit <martina.vit@cern.ch>
2019-10-08 11:03:58 +0200

    Update extractPuWeights.cc

--- src/extractPuWeights.cc
+++ src/extractPuWeights.cc
@@ -119,6 +119,7 @@ void extractPuWeights(const Sample& sample){
             //if (sample.is2018() || year == "2018"){
              //   denominator = rebinHistogram(denominator, 50);
           //  }
+            denominator = rebinHistogram(denominator, 50);
             std::cout<<sample.is2016()<<" "<< sample.is2017()<< " "<<sample.is2018()<<std::endl;
             std::cout<< "numerator: "<<numerator->GetNbinsX()<<std::endl;
             std::cout<< "denominator: "<<denominator->GetNbinsX()<<std::endl;
