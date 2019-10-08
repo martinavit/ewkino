df1a2c4e54aa099fc8bd1f6e4464ebef443270dc
martinavit <martina.vit@cern.ch>
2019-10-08 15:49:42 +0200

    Update extractPuWeights.cc

--- src/extractPuWeights.cc
+++ src/extractPuWeights.cc
@@ -77,11 +77,11 @@ void extractPuWeights(const Sample& sample){
     mcPuDist->Scale(1./mcPuDist->GetSumOfWeights());
 
     //pu weights
-    std::shared_ptr<TH1D> puWeights[8][3];
+    std::shared_ptr<TH1D> puWeights[13][3];
    
     //categorization by year, run era and ucertainty
     const std::vector< std::string > eras2016 = {"2016Inclusive", "2016B", "2016C", "2016D", "2016E", "2016F", "2016G", "2016H"};
-   // const std::vector< std::string > eras2017  = {"2018Inclusive", "2018B", "2018C", "2018D", "2018E"};
+    const std::vector< std::string > eras2017  = {"2018Inclusive", "2018B", "2018C", "2018D", "2018E"};
     //const std::vector< std::string > eras2017  = {"2017Inclusive", "2017B", "2017C", "2017D", "2017E","2017F"};
     std::vector< std::string > allEras = eras2016;
     //allEras.insert(allEras.begin(), eras2017.begin(), eras2017.end() );
@@ -119,7 +119,7 @@ void extractPuWeights(const Sample& sample){
             //if (sample.is2018() || year == "2018"){
              //   denominator = rebinHistogram(denominator, 50);
           //  }
-            denominator = rebinHistogram(denominator, 50);
+            //denominator = rebinHistogram(denominator, 50);
             std::cout<<sample.is2016()<<" "<< sample.is2017()<< " "<<sample.is2018()<<std::endl;
             std::cout<< "numerator: "<<numerator->GetNbinsX()<<std::endl;
             std::cout<< "denominator: "<<denominator->GetNbinsX()<<std::endl;
@@ -148,7 +148,7 @@ void extractPuWeights(const Sample& sample){
 int main(int argc, char* argv[]){
 
     //list of samples
-    std::vector< Sample > sampleVector = readSampleList( "/user/mvit/CMSSW_9_4_4/src/HNL_analysis/sampleLists/2016_new.txt", "/pnfs/iihe/cms/store/user/mvit/samples/FINAL/2016"  );
+    std::vector< Sample > sampleVector = readSampleList( "/user/mvit/CMSSW_9_4_4/src/HNL_analysis/sampleLists/2018.txt", "/pnfs/iihe/cms/store/user/mvit/samples/FINAL/2018"  );
     //read sample lists from txt 
     //std::vector< Sample > sampleVector2017 = readSampleList( "sampleLists/samples_dilepCR_2017.txt", "/pnfs/iihe/cms/store/user/wverbeke/ntuples_ewkino" );
    /* for( auto& samp : sampleVector2017 ){
