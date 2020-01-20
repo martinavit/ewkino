cfd36086266aea82cc31dbb314459ad6616aa89f
martinavit <martina.vit@cern.ch>
2020-01-20 13:31:59 +0100

    Update extractPuWeights.cc

--- src/extractPuWeights.cc
+++ src/extractPuWeights.cc
@@ -149,7 +149,7 @@ void extractPuWeights(const Sample& sample){
 int main(int argc, char* argv[]){
 
     //list of samples
-    std::vector< Sample > sampleVector = readSampleList( "/user/mvit/CMSSW_9_4_4/src/HNL_analysis/sampleLists/2018.txt", "/pnfs/iihe/cms/store/user/mvit/samples/FINAL/2018"  );
+    std::vector< Sample > sampleVector = readSampleList( "/user/mvit/CMSSW_9_4_4/src/HNL_analysis/sampleLists/forPUweights_2016.txt", "/pnfs/iihe/cms/store/user/mvit/samples/FINAL/2016"  );
     //read sample lists from txt 
     //std::vector< Sample > sampleVector2017 = readSampleList( "sampleLists/samples_dilepCR_2017.txt", "/pnfs/iihe/cms/store/user/wverbeke/ntuples_ewkino" );
    /* for( auto& samp : sampleVector2017 ){
