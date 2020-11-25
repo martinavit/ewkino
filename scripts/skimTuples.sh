6381d672502b6c845b7b2d25756ce259218744a9
martinavit <martina.vit@cern.ch>
2020-11-25 14:33:37 +0100

    for dirac

--- scripts/skimTuples.sh
+++ scripts/skimTuples.sh
@@ -60,8 +60,8 @@ skimSample(){                                           #function to skim one sa
 baseFolder=/pnfs/iihe/cms/store/user/tomc/heavyNeutrino
 
 cd $baseFolder
-folderTestmu=SingleMuon/crab_Run2016B-17Jul2018_ver2-v1_displaced_2016_v2
-folderTeste=HeavyNeutrino_trilepton_M-1*e_massiveAndCKM_LO*/*17*displaced_signals_v4
+folderTestmu=HeavyNeutrino_trilepton_M-*mu_Dirac_massiveAndCKM_LO*/*17*displaced_signals_v4
+folderTeste=HeavyNeutrino_trilepton_M-*e_Dirac_massiveAndCKM_LO*/*17*displaced_signals_v4
 #folderTest=SingleMuon/*displaced_2018_v2
 folderTomMC=*/*displaced_2018_v1
 foldersData=*/*2016_legacy9March
